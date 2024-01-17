60a779237d854fdd3325a85a5b6ee98b5018a645
SeifHecker2099 <102517370+SeifHossam17@users.noreply.github.com>
2024-01-17 02:37:14 +0200

    Update and rename omni_gta4lve.mk to twrp_gta4lve.mk

--- twrp_gta4lve.mk
+++ twrp_gta4lve.mk
@@ -1,22 +1,18 @@
-#
 # Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
 # SPDX-License-Identifier: Apache-2.0
-#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from gta4lve device
 $(call inherit-product, device/samsung/gta4lve/device.mk)
 
 PRODUCT_DEVICE := gta4lve
-PRODUCT_NAME := omni_gta4lve
+PRODUCT_NAME := twrp_gta4lve
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T509
 PRODUCT_MANUFACTURER := samsung
